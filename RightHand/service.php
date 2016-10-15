6d01b74d7992596f7a083c13e31c692d03024227
Sowmya <kalyan.cmu@gmail.com>
2016-10-15 03:33:21 -0400

    trying new gear

--- RightHand/service.php
+++ RightHand/service.php
@@ -6,13 +6,17 @@ $username = 'adminR4Ltur9';
 $password = 'zjQeA1RT-wYr';
 
 // Create database connection using PHP Data Object (PDO)
-$db = new PDO("mysql://$OPENSHIFT_MYSQL_DB_HOST:$OPENSHIFT_MYSQL_DB_PORT;dbname=RightHand", $username, $password);
+$db = new PDO("mysql://$OPENSHIFT_MYSQL_DB_HOST:$OPENSHIFT_MYSQL_DB_PORT;dbname=php", $username, $password);
+
+var_dump($db);
 
 // Identify name of table within database
 $table = 'tb_provider';
 $serviceType=$_REQUEST['serviceType'];
 // Create the query - here we grab everything from the table
 $stmt = $db->query('SELECT * from '.$table." where profession='".$serviceType."'");
+
+var_dump($stmt);
 
 // Close connection to database
 $db = NULL;
