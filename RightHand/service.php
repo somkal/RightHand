a0d4603bbca1f73c19e7cd93b20f2601c420cd8a
Sowmya <kalyan.cmu@gmail.com>
2016-10-15 05:02:37 -0400

    trying new gear

--- RightHand/service.php
+++ RightHand/service.php
@@ -1,18 +1,32 @@
 <?php
 
-// Enter username and password
-$username = 'adminR4Ltur9';
-$password = 'zjQeA1RT-wYr';
+// // Enter username and password
+// $username = 'adminR4Ltur9';
+// $password = 'zjQeA1RT-wYr';
 
 define('DB_HOST', getenv('OPENSHIFT_MYSQL_DB_HOST'));
 define('DB_PORT', getenv('OPENSHIFT_MYSQL_DB_PORT'));
+define('DB_USER', getenv('OPENSHIFT_MYSQL_DB_USERNAME'));
+define('DB_PASS', getenv('OPENSHIFT_MYSQL_DB_PASSWORD'));
 
-// Create database connection using PHP Data Object (PDO)
-$db = new mysqli(DB_HOST,$username,$password,'php');
+if(!DB_HOST){
+	$dbhost = 'localhost';
+	$dbuser = 'root';
+	$dbpass = 'root';
+	$dbname = 'RightHand';	
+}else{
+	$dbhost = DB_HOST;
+	$dbuser = DB_USER;
+	$dbpass = DB_PASS;
+	$dbname = 'php';
+}
+
+// Create database connection without using PHP Data Object (PDO)
+$db = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
 
 // Identify name of table within database
 $table = 'tb_provider';
-$serviceType=$_REQUEST['serviceType'];
+$serviceType = $_REQUEST['serviceType'];
 
 // Create the query - here we grab everything from the table
 $stmt = $db->query('SELECT * from '.$table." where profession='".$serviceType."'");
@@ -21,6 +35,6 @@ $stmt = $db->query('SELECT * from '.$table." where profession='".$serviceType."'
 $db = NULL;
 header('Content-Type: application/json');
 $results = $stmt->fetch_all(MYSQLI_ASSOC);
-$json=json_encode($results);
+$json = json_encode($results);
 echo $json;
 exit();
